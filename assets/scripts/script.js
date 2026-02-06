22a75e8e29bff2f403d9447f8bcafbaf8d638119
Cayo Bortolotti <cayogomes290@gmail.com>
2026-02-06 12:31:54 -0300

    rodape e botão credenciamento finalizado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const seta = document.getElementById('seta-mestre');
-    const grid = document.getElementById('wrapper-grid-trilhas');
-
-    seta.addEventListener('click', function() {
-        // O próprio Bootstrap cuida da suavidade com o método .collapse('toggle')
-        $(grid).collapse('toggle');
-    });
-});
 
 const setaUsuario = document.querySelector('.seta-usuario');
 const dropdownUsuario = document.querySelector('.dropdown-usuario');
+document.addEventListener('DOMContentLoaded', function() {
+    const seta = document.getElementById('seta-mestre');
+    const grid = document.getElementById('wrapper-grid-trilhas');
 
 setaUsuario.addEventListener('click', (e) => {
     e.stopPropagation(); // não deixa o clique subir
@@ -19,4 +13,9 @@ setaUsuario.addEventListener('click', (e) => {
 // fecha ao clicar fora
 document.addEventListener('click', () => {
     dropdownUsuario.classList.remove('ativo');
+});
+    seta.addEventListener('click', function() {
+        // O próprio Bootstrap cuida da suavidade com o método .collapse('toggle')
+        $(grid).collapse('toggle');
+    });
 });
